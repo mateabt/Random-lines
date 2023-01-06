8ab0a3285436faa777733ea9a33da108ca494d24
Matea <mateabt@gmail.com>
2023-01-06 13:54:56 +0100

    dr nacin

--- konveksne_objekte.py
+++ konveksne_objekte.py
@@ -8,12 +8,14 @@ from typing import Union, Tuple
 import random
 import math
 
+
 num=15
 points = np.random.uniform(0, 10, size=(num, 2))  # Random points in 2-D (15 between 0 ans 10)
 hull = ConvexHull(points) 
 
-points1 = npi.difference(points,points[hull.vertices]) #tocke brez tiste na ovojnici C1
+points1 = npi.difference(points,points[hull.vertices]) #tocke brez tiste na ovojnici vecje C
 hull1= ConvexHull(points1) 
+points2= points1[hull1.vertices] # tocke na C1 notranji
 
 #Bounding box
 bbox = [hull.min_bound, hull.max_bound]
@@ -24,36 +26,52 @@ num_miss =0
 
 
 
-def magnitude(vector):
-   return np.sqrt(np.dot(np.array(vector),np.array(vector)))
-
-def norm(vector):
-   return np.array(vector)/magnitude(np.array(vector))
-
+#def magnitude(vector):
+#   return np.sqrt(np.dot(np.array(vector),np.array(vector)))
+#
+#def norm(vector):
+#   return np.array(vector)/magnitude(np.array(vector))
+#
 
 # ne dela pravilno
 def lineRayIntersectionPoint(rayOrigin, rayDirection, point1, point2):
     rayOrigin = np.array(rayOrigin, dtype=np.float)
-    rayDirection = np.array(norm(rayDirection), dtype=np.float)
+    rayDirection = np.array(rayDirection, dtype=np.float)
     point1 = np.array(point1, dtype=np.float)
     point2 = np.array(point2, dtype=np.float)
+    
+    rayOrigin = np.append(1,rayOrigin)
+    rayDirection = np.append(1,rayDirection)
+    point1=np.append(1,point1)
+    point2=np.append(1,point2)
+    v1=np.linalg.det([rayOrigin,rayDirection,point1])
+    v2=np.linalg.det([rayOrigin,rayDirection,point2])
+    z1=np.linalg.det([point1,point2,rayOrigin])
+    z2=np.linalg.det([point1,point2,rayDirection])
+    if z1*z2<=0 and v1*v2<=0:
+        return True
+    return False
 
-    v1 = rayOrigin - point1
-    v2 = point2 - point1
-    v3 = np.array([-rayDirection[1], rayDirection[0]])
-    t1 = np.cross(v2, v1) / np.dot(v2, v3)
-    t2 = np.dot(v1, v3) / np.dot(v2, v3)
-    if t1 >= 0.0 and t2 >= 0.0 and t2 <= 1.0:
-        return [rayOrigin + t1 * rayDirection]
-    return []
-
-
-
+    
+    
+    
+#
+#    v1 = rayOrigin - point1
+#    v2 = point2 - point1
+#    v3 = np.array([-rayDirection[1], rayDirection[0]])
+#    t1 = np.cross(v2, v1) / np.dot(v2, v3)
+#    t2 = np.dot(v1, v3) / np.dot(v2, v3)
+#    if t1 >= 0.0 and t2 >= 0.0 and t2 <= 1.0:
+#        return [rayOrigin + t1 * rayDirection]
+#    return []
+#
+#
+#
 
 n = num_tries # stevilo premic
 b= np.empty((n, 2))
 for i in range(n):
-    #nakljucna tocka v C skozi katero grejo
+    #nakljucna tocka v C skozi katero grejo (vecji)
     b[i] = np.array([np.random.uniform(bbox[0][0], bbox[1][0]), np.random.uniform(bbox[0][1], bbox[1][1])])
     #Preverimo ce je res v c           
     while Path(hull.points[hull.vertices] ).contains_point(b[i]) == False:
@@ -64,17 +82,22 @@ theta=[]
 for i in range(len(b)):
                theta=np.append(theta,np.random.uniform(0,2*np.pi))
 
+
 for i in range(len(b)):
     r = b[i]
-    d = ((math.cos(theta[i]),math.sin(theta[i])))
-    for j in range(len(points1)):
-        z1 =points1[j-1]
-        z2 = points1[j]
-        if len(lineRayIntersectionPoint(r,d,z1,z2)) == 1:
+    d1 = (math.cos(theta[i]),math.sin(theta[i])) # v eno smer
+    d2 =(-math.cos(theta[i]),-math.sin(theta[i])) # v dr smer
+    
+    
+    for j in range(len(points2)):
+        z1 =points2[j-1]
+        z2 = points2[j]
+
+        if lineRayIntersectionPoint(r,d1,z1,z2) or lineRayIntersectionPoint(r,d2,z1,z2):
             num_hits+=1
             break
-        break
-    
+        
+
 print(num_hits)   
 
 
